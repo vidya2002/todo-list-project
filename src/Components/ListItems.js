a263e7662bdf703d05374bdce10fd83155fdc3a7
Vidya Sonawane <vidyasonawane0002@gmail.com>
2023-03-05 23:07:23 +0530

    first commit

---
+++ src/Components/ListItems.js
@@ -0,0 +1,30 @@
+import "../Components/ListItems.css";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function ListItems(props) {
+  const items = props.items;
+  const ListItems = items.map((item) => {
+    return (
+      <div className="list" key={"item.key"}>
+        <p>
+          <input type="text" id={item.key} value={item.text} onChange = {
+            (e) =>{
+              props.setUpdate(e.target.value, item.key)
+            }
+          } />
+          <span>
+            <FontAwesomeIcon
+              className="faicons"
+              icon="trash"
+              onClick={() => props.deleteItem(item.key)}
+            />
+          </span>
+        </p>
+      </div>
+    );
+  });
+  return <div>{ListItems}</div>;
+}
+
+export default ListItems;
